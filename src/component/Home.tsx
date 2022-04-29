bfffde5b71441393e368f4af934337fb1bf3d5bf
vedant jaiswal <vedantjaiswal1@gmail.com>
2022-04-29 16:25:42 +0530

    component add

---
+++ src/component/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+
+export const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <h2>Home</h2>
+      </div>
+      <Footer />
+    </div>
+  );
+};
