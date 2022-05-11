d515c41079a4a0b43d590c5fd66b6fd1bce88092
vedant <vedantjaiswal@gmail.com>
2022-05-11 19:03:51 +0530

    validation with the formik and yup apply

--- src/component/CRUD/CreateUser/CreateUser.tsx
+++ src/component/CRUD/CreateUser/CreateUser.tsx
@@ -1,58 +1,77 @@
 import React, { useState } from "react";
-import { Card, Button, FormGroup, FormLabel, FormControl } from "react-bootstrap";
-import {Formik, Form} from "formik";
+import { Card, Button, FormGroup, FormLabel, FormControl, FormText } from "react-bootstrap";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+const UserSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too short!")
+    .max(50, "Too Long!")
+    .required("Name is required"),
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Email is required")
+});
+
 export const CreateUser = ({ addUser }: any) => {
   const initialState = {
     id: null,
     name: "",
-    email: "",
+    email: ""
   };
   const [user, setUser] = useState(initialState);
 
-  const handleInputChange = (event: any) => {
-    const { name, value } = event.target;
-    setUser({ ...user, [name]: value });
+  const onSubmitForm = (values: any) => {
+    addUser(values);
+    setUser(initialState);
   };
 
-  const onSubmitForm = (event: any) => {
-    event.preventDefault();
-    if (!user.name || !user.email) return;
-    addUser(user);
-    setUser(initialState);
-    console.log(user);
-  };
   return (
     <React.Fragment>
       <Card border="primary">
         <Card.Body>
           <h3>Create User</h3>
-          <Form onSubmit={onSubmitForm}>
-            <FormGroup className="mb-3" controlId="formBasicName">
-              <FormLabel>Name</FormLabel>
-              <FormControl
-                type="text"
-                placeholder="Enter name"
-                name="name"
-                value={user.name}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
+          <Formik
+            initialValues={initialState}
+            validationSchema={UserSchema}
+            onSubmit={(values, actions) => {
+              onSubmitForm(values);
+              actions.resetForm({});
+              console.log(values);
+            }}
+          >
+            {({ errors, touched, values, handleChange }) => (
+              <Form>
+                <FormGroup className="mb-3" controlId="formBasicName">
+                  <FormLabel>Name</FormLabel>
+                  <FormControl
+                    type="text"
+                    placeholder="Enter name"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                  />
+                  {errors.name && touched.name ? <FormText className="text-danger">{errors.name}</FormText> : null}
+                </FormGroup>
 
-            <FormGroup className="mb-3" controlId="formBasicEmail">
-              <FormLabel>Email</FormLabel>
-              <FormControl
-                type="email"
-                placeholder="Enter Email"
-                name="email"
-                value={user.email}
-                onChange={handleInputChange}
-              />
-            </FormGroup>
+                <FormGroup className="mb-3" controlId="formBasicEmail">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl
+                    type="email"
+                    placeholder="Enter Email"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                  />
+                  {errors.email && touched.email ? <FormText className="text-danger">{errors.email}</FormText> : null}
+                </FormGroup>
 
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            )}
+          </Formik>
         </Card.Body>
       </Card>
     </React.Fragment>
